db32554947d5c05993172a15b5a1f88b96ece116
Conner DiPaolo <cdipaolo96@gmail.com>
2015-07-21 22:31:24 -0700

    Softmax on MNIST set working with 68.09% accuracy

--- mnist-softmax/mnist-softmax.go
+++ mnist-softmax/mnist-softmax.go
@@ -7,8 +7,18 @@ import (
 	"github.com/cdipaolo/goml/base"
 	"github.com/cdipaolo/goml/linear"
 
+	"github.com/cheggaaa/pb"
 	mnist "github.com/petar/GoMNIST"
 )
+
+func newBar(count int64) *pb.ProgressBar {
+	bar := pb.New64(count)
+
+	bar.ShowTimeLeft = true
+	bar.ShowSpeed = true
+
+	return bar
+}
 
 // maxI returns the index of the maximum value
 // of a slice of float64's
@@ -33,30 +43,30 @@ func main() {
 
 	fmt.Printf("MNIST dataset loaded!\n\t%v Training Examples\n\t%v Test Examples\n", len(train.Images), len(test.Images))
 
-	tr := train.Sweep()
-
 	stream := make(chan base.Datapoint, 1000)
 	errors := make(chan error, 200)
 
 	now := time.Now()
 	fmt.Printf("Starting Loading/Training at %v\n", now)
 
-	model := linear.NewSoftmax(base.StochasticGA, 0.1, 0, 10, 0, nil, nil, 784)
+	model := linear.NewSoftmax(base.StochasticGA, 1e-5, 0, 10, 0, nil, nil, 784)
 
 	go model.OnlineLearn(errors, stream, func(theta [][]float64) {})
 
 	// push data onto the stream while waiting for errors
 	go func() {
-		bytes, label, present := tr.Next()
-		fmt.Println("\n\n", len(bytes), int(label), []uint8(bytes))
-		ct := 1
+		size := train.Count()
+		bytes, label := train.Get(0)
 
-		for present {
+		bar := newBar(int64(size))
+		bar.Start()
+		count := 0
+		for count = 0; count < size; count++ {
 			x := make([]float64, len(bytes))
 			y := []float64{float64(label)}
 
 			for i := range bytes {
-				x[i] = float64(bytes[i]) / 256
+				x[i] = float64(bytes[i]) / 255
 			}
 
 			stream <- base.Datapoint{
@@ -64,15 +74,12 @@ func main() {
 				Y: y,
 			}
 
-			bytes, label, present = tr.Next()
-			ct++
+			bytes, label = train.Get(count)
 
-			if ct%500 == 0 {
-				print(".")
-			}
+			bar.Increment()
 		}
 
-		fmt.Printf("Loaded %v examples onto the data stream\n", ct)
+		bar.FinishPrint(fmt.Sprintf("Loaded %v examples onto the data stream", count))
 
 		close(stream)
 	}()
@@ -99,20 +106,17 @@ func main() {
 
 	bytes, label := test.Get(0)
 
-	x := make([]float64, len(bytes))
-
-	for i := range bytes {
-		x[i] = float64(bytes[i]) / 256
-	}
-
 	now = time.Now()
 	fmt.Printf("Starting Testing at %v\n", now)
+
+	bar := newBar(int64(testEx))
+	bar.Start()
 
 	for ; count < testEx; count++ {
 		x := make([]float64, len(bytes))
 
 		for i := range bytes {
-			x[i] = float64(bytes[i]) / 256
+			x[i] = float64(bytes[i]) / 255
 		}
 
 		guess, err := model.Predict(x)
@@ -127,15 +131,11 @@ func main() {
 			errMap[class]++
 		}
 
-		if count%500 == 0 {
-			// print(".")
-			fmt.Printf("%v percent completed\n", 100*(float64(count)/float64(testEx)))
-		}
-
+		bar.Increment()
 		bytes, label = test.Get(count)
 	}
 
-	fmt.Printf("Stopped Testing at %v\n\tTraining Time: %v\n", time.Now(), time.Now().Sub(now))
+	bar.FinishPrint(fmt.Sprintf("Stopped Testing at %v\n\tTraining Time: %v\n", time.Now(), time.Now().Sub(now)))
 
 	accuracy := 100 * (1 - float64(wrong)/float64(count))
 	fmt.Printf("Accuracy: %v percent\n\tPoints Tested: %v\n\tMisclassifications: %v\n\tMisclassifications by Digit: %v\n", accuracy, count, wrong, errMap)
