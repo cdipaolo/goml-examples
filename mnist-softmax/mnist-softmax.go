12d61145e4ab64345f34634ee6070cfcd82924f1
Conner DiPaolo <cdipaolo96@gmail.com>
2015-07-22 09:13:55 -0700

    Added batch GA softmax testing as well

--- mnist-softmax/mnist-softmax.go
+++ mnist-softmax/mnist-softmax.go
@@ -12,6 +12,9 @@ import (
 )
 
 const (
+	// Epochs defines the number of
+	// iterations the model will go
+	// through the data
 	Epochs int64 = 1
 )
 
@@ -36,24 +39,16 @@ func maxI(array []float64) int {
 	return i
 }
 
-func main() {
-
-	fmt.Printf("Loading MNIST dataset into memory...\n")
-
-	train, test, err := mnist.Load("./")
-	if err != nil {
-		panic(fmt.Sprintf("Error loading training set!\n\t%v\n\n", err))
-	}
-
-	fmt.Printf("MNIST dataset loaded!\n\t%v Training Examples\n\t%v Test Examples\n", len(train.Images), len(test.Images))
+func onlineLearn(train *mnist.Set) *linear.Softmax {
+	var err error
 
 	stream := make(chan base.Datapoint, 1000)
 	errors := make(chan error, 200)
 
 	now := time.Now()
-	fmt.Printf("Starting Loading/Training at %v\n", now)
+	fmt.Printf("Starting Loading/Training of < Online Softmax > at %v\n", now)
 
-	model := linear.NewSoftmax(base.StochasticGA, 1e-5, 0, 10, 0, nil, nil, 784)
+	model := linear.NewSoftmax(base.StochasticGA, 1e-5, 10, 10, 0, nil, nil, 784)
 
 	go model.OnlineLearn(errors, stream, func(theta [][]float64) {})
 
@@ -71,7 +66,7 @@ func main() {
 				y := []float64{float64(label)}
 
 				for i := range bytes {
-					// x[i] = float64(bytes[i]) / 255
+					//x[i] = float64(bytes[i]) / 255
 					if bytes[i] > uint8(200) {
 						x[i] = 1
 					}
@@ -88,7 +83,7 @@ func main() {
 			}
 		}
 
-		bar.FinishPrint(fmt.Sprintf("Loaded %v examples onto the data stream\n\tRepeated through %v epochs", count, Epochs))
+		bar.FinishPrint(fmt.Sprintf("Loaded %v examples onto the data stream of < Online Softmax >\n\tRepeated through %v epochs", count, Epochs))
 
 		close(stream)
 	}()
@@ -105,8 +100,64 @@ func main() {
 	}
 
 	// now the model is trained! Test it!
-	fmt.Printf("Stopped Loading/Training at %v\n\tTraining Time: %v\n", time.Now(), time.Now().Sub(now))
+	fmt.Printf("Stopped Loading/Training of < Online Softmax > at %v\n\tTraining Time: %v\n", time.Now(), time.Now().Sub(now))
 
+	return model
+}
+
+func batchLearn(train *mnist.Set) *linear.Softmax {
+	var err error
+	x := [][]float64{}
+	y := []float64{}
+
+	size := train.Count()
+	bytes, label := train.Get(0)
+
+	bar := newBar(int64(size))
+	bar.Start()
+	count := 0
+
+	now := time.Now()
+	fmt.Printf("Starting Loading of < Batch Softmax > at %v\n", now)
+
+	for count = 0; count < size; count++ {
+		inp := make([]float64, len(bytes))
+		out := float64(label)
+
+		for i := range bytes {
+			//x[i] = float64(bytes[i]) / 255
+			if bytes[i] > uint8(200) {
+				inp[i] = 1
+			}
+		}
+
+		x = append(x, inp)
+		y = append(y, out)
+
+		bytes, label = train.Get(count)
+
+		bar.Increment()
+	}
+
+	bar.FinishPrint(fmt.Sprintf("Loaded %v examples into < Batch Softmax >\n\tTook %v", count, time.Now().Sub(now)))
+
+	model := linear.NewSoftmax(base.StochasticGA, 1e-5, 10, 10, int(Epochs), x, y)
+	fmt.Println(model.Parameters[0])
+
+	now = time.Now()
+	fmt.Printf("Starting Training of < Batch Softmax > at %v over %v epochs\n", now, Epochs)
+
+	err = model.Learn()
+	if err != nil {
+		panic(fmt.Sprintf("Error learning on < Batch Softmax >!\n\t%v", err))
+	}
+
+	fmt.Printf("Stopped Training < Batch Softmax > at %v\n\tTraining Time: %v\n", time.Now(), time.Now().Sub(now))
+
+	return model
+}
+
+func eval(model *linear.Softmax, test *mnist.Set, method string) {
 	testEx := test.Count()
 
 	var count int
@@ -115,8 +166,8 @@ func main() {
 
 	bytes, label := test.Get(0)
 
-	now = time.Now()
-	fmt.Printf("Starting Testing at %v\n", now)
+	now := time.Now()
+	fmt.Printf("Starting Testing of < %v > at %v\n", method, now)
 
 	bar := newBar(int64(testEx))
 	bar.Start()
@@ -126,7 +177,7 @@ func main() {
 
 		for i := range bytes {
 			//x[i] = float64(bytes[i]) / 255
-			if bytes[i] > uint8(200) {
+			if bytes[i] > uint8(220) {
 				x[i] = 1
 			}
 		}
@@ -147,8 +198,28 @@ func main() {
 		bytes, label = test.Get(count)
 	}
 
-	bar.FinishPrint(fmt.Sprintf("Stopped Testing at %v\n\tTraining Time: %v\n", time.Now(), time.Now().Sub(now)))
+	bar.FinishPrint(fmt.Sprintf("Stopped Testing of < %v > at %v\n\tTraining Time: %v\n", method, time.Now(), time.Now().Sub(now)))
 
 	accuracy := 100 * (1 - float64(wrong)/float64(count))
-	fmt.Printf("Accuracy: %v percent\n\tPoints Tested: %v\n\tMisclassifications: %v\n\tMisclassifications by Digit: %v\n", accuracy, count, wrong, errMap)
+	fmt.Printf("Accuracy of < %v >: %v percent\n\tPoints Tested: %v\n\tMisclassifications: %v\n\tMisclassifications by Digit: %v\n", method, accuracy, count, wrong, errMap)
+}
+
+func main() {
+
+	fmt.Printf("Loading MNIST dataset into memory...\n")
+
+	train, test, err := mnist.Load("./")
+	if err != nil {
+		panic(fmt.Sprintf("Error loading training set!\n\t%v\n\n", err))
+	}
+
+	fmt.Printf("MNIST dataset loaded!\n\t%v Training Examples\n\t%v Test Examples\n", len(train.Images), len(test.Images))
+
+	online := onlineLearn(train)
+
+	eval(online, test, "Online Softmax")
+
+	batch := batchLearn(train)
+
+	eval(batch, test, "Batch Softmax")
 }
